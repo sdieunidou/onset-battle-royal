e23e7c5bee7066d1b3210f318ad0e55a47d3a0b6
Seb <s.dieunidou@gmail.com>
2019-12-18 14:21:06 +0100

    remove unused require

--- boot/server.lua
+++ boot/server.lua
@@ -1,7 +1,6 @@
 local package_name = GetPackageName()
 local battleManager = require( ( 'packages/%s/battle/server/battle' ):format( package_name ) )
 local pprint = require( ( 'packages/%s/utils/server/pprint' ):format( package_name ) )
-local vector = require( ( 'packages/%s/utils/server/vector' ):format( package_name ) )
 
 BR = BR or {}
 
@@ -12,7 +11,7 @@ BR.Config.DIMENSION_BATTLE = 100
 BR.Config.MAX_TIME = 2 * 60
 BR.Config.TIME_BY_ZONE = 30
 BR.Config.ROUNDS = BR.Config.MAX_TIME / BR.Config.TIME_BY_ZONE
-BR.Config.MIN_PLAYERS = 0
+BR.Config.MIN_PLAYERS = 1
 BR.Config.TIMER_CHECK_PLAYERS = 5
 BR.Config.MAX_NUMBER_PLAYERS_BY_BATTLE = GetMaxPlayers()
 BR.Config.ZONE_RADIUS = 2000
