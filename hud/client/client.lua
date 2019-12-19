3bc8e2d5ead14c568d269ebf68b7f0cacecb93d0
Seb <s.dieunidou@gmail.com>
2019-12-19 03:19:31 +0100

    add icon in HUD when voice is activated

--- hud/client/client.lua
+++ hud/client/client.lua
@@ -4,16 +4,31 @@ local _ = function(k,...) return ImportPackage("i18n").t(GetPackageName(),k,...)
 local minimap
 
 function OnPackageStart()
+    ShowWeaponHUD(true)
+
     minimap = CreateWebUI(0, 0, 0, 0, 0, 32)
     SetWebVisibility(minimap, WEB_HITINVISIBLE)
     SetWebAnchors(minimap, 0, 0, 1, 1)
     SetWebAlignment(minimap, 0, 0)
     SetWebURL(minimap, "http://asset/battleroyal/hud/minimap/minimap.html")
-    ShowWeaponHUD(true)
+
+    SpeakingHud = CreateWebUI( 0, 0, 0, 0, 0, 50 )
+    LoadWebFile( SpeakingHud, "http://asset/battleroyal/hud/speaking/hud.html" )
+    SetWebAlignment( SpeakingHud, 0, 0 )
+    SetWebAnchors( SpeakingHud, 0, 0, 1, 1 )
+    SetWebVisibility( SpeakingHud, WEB_HITINVISIBLE )
 end
 AddEvent("OnPackageStart", OnPackageStart)
 
 AddEvent( "OnGameTick", function()
+    --Speaking icon check
+    local player = GetPlayerId()
+    if IsPlayerTalking(player) then
+        SetWebVisibility(SpeakingHud, WEB_HITINVISIBLE)
+    else
+        SetWebVisibility(SpeakingHud, WEB_HIDDEN)
+    end
+
     --Minimap refresh
     local x, y, z = GetCameraRotation()
     local px,py,pz = GetPlayerLocation()
@@ -28,5 +43,4 @@ function SetHUDMarker(name, h, r, g, b)
         ExecuteWebJS(minimap, "SetHUDMarker(\""..name.."\", "..h..", "..r..", "..g..", "..b..");");
     end
 end
-
 AddRemoteEvent("SetHUDMarker", SetHUDMarker)
